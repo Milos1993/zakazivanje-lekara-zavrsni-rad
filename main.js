623757909afaddc430e74d1a59e2372151ca6251
Milos1993 <djordjevicmilos993@gmail.com>
2019-04-15 21:45:44 +0200

    Prva verzija

---
+++ main.js
@@ -0,0 +1,36 @@
+
+fetch('https://randomuser.me/api/?results=6')
+.then(response => response.json())
+.then(podaci => prikaziDoktora(podaci))
+
+
+function prikaziDoktora(korisnici) {
+    const doktori = document.getElementById('doktori');
+    let kartica = ``;
+
+    for(let i = 0; i < korisnici.results.length; i++) {
+        const doktor = korisnici.results[i];
+
+        kartica += `
+        
+        <div class="col-sm-12 col-md-4 col-lg-4>
+        <div class="card">
+        <div class="card-body">
+            <img src="${doktor.picture.large}" class="card-img-top" alt="${doktor.name.first}">
+          <h5 class="card-title velikoSlovo">${doktor.name.first} ${doktor.name.last}</h5>
+          <p class="card-text">Email: ${doktor.email}</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+      </div>
+        
+        `
+    }
+
+    doktori.innerHTML = kartica
+}
+
+
+
+
+
